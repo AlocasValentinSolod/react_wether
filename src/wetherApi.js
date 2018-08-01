a5519088d82ca9182bda7e8d7cbc171c8b6e09aa
ValentinSolod <v.solodcovschi@price.md>
2018-08-01 11:34:53 +0300

    work_day2

--- src/wetherApi.js
+++ src/wetherApi.js
@@ -1,38 +1,41 @@
 import React, { Component } from 'react'
 
+
 class wetherApi extends Component {
     constructor () {
         super()
         this.state = {
-            data: null,
-            isLoading: false,
-            error: null,
+            isLoading: true
         }
     }
 
     componentDidMount () {
         const API = 'http://api.openweathermap.org/data/2.5/box/city?bbox=12,32,15,37,10'
         const APPID = '&APPID=a3f49b01aa111337a90b621b60ca5e18'
+        console.log(this.ready)
         this.setState( { isLoading: true } )
         fetch( API + APPID )
             .then( response => {
                 if ( response.ok ) {
-                    return response.json()
+                    return response.json();
                 } else {
                     throw new Error( 'Something  wrong ...' )
                 }
             } )
             .then( data => {
                 const cityName = data.list.map( city => city.name )
-                this.setState( { data: cityName, isLoading: false } )
+                cityName ? this.setState( { data: cityName, isLoading: false } ) : new Error()
             } )
-            .catch( error => this.setState( { error, isLoading: false } ) )
+            .catch( error => error ? this.setState( { error, isLoading: true } ) : this.setState( { error, isLoading: true } ) )
     }
 
     render () {
+        this.ready = true
         return (
+
             <ul>
-                { this.state.data ? this.state.data.map( ( city ) => { return ( <li> { city } </li> ) } ) : null
+                {
+                    this.state.isLoading ? <p> isLoading </p> : this.state.data ? this.state.data.map( ( city, key ) => { return ( <li key={ key }> { city } </li> ) } ) : null
                 }
             </ul>
         )
